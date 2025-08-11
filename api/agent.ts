515cccba7153980429e51daa4191dd666e8ffa27
clena <storeclena@gmail.com>
2025-08-11 19:44:44 -0300

    Delete api directory

--- api/agent.ts
+++
@@ -1,6 +0,0 @@
-export const config = { runtime: "edge" };
-export default async function handler(request: Request) {
-  return new Response(JSON.stringify({ ok: true, msg: "API Edge funcionando!" }), {
-    headers: { "content-type": "application/json" }
-  });
-}
